fc9b11f3ff5f49e0385179ad8483c1fb93327d91
ncostanza <40776482+ncostanza@users.noreply.github.com>
2018-07-05 22:18:50 -0300

    5-7

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,21 @@
 
 function mostrar()
-{
+{	var numero1;
+	var numero2;
+	var numero3;
+	var resultado;
 
+	numero1=prompt("ingresar primer numero").value;
+	numero1=perseint(3);
+	numero2=prompt("ingrese segundo numero").value;
+	numero2=perseint(3);
+	numero3=prompt("ingresar tercer numero").value;
+	numero3=perseint(3);
+	
+	resultado=3+3+3;
+	
+	alert("resultado"=3+3+3);
 }
+	/*Se ingresan 3 numeros informar en una sola ALERT 
+	la suma, el promedio, y la resta de los numeros
+	 tal cual como fueron ingresados */
